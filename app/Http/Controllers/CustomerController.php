2d7de8074994a8f83482e802f45ef989ce3145fd
sanchita <paul.sanchitaa@gmail.com>
2025-05-07 14:42:30 +0600

    [feature/ONKO-15/customers-table] pagination updated

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -10,7 +10,7 @@ class CustomerController extends Controller
     public function index()
     {
         return Inertia::render('customers/index', [
-            'customers' => Customer::paginate(100)
+            'customers' => Customer::paginate(25)
         ]);
     }
 }
