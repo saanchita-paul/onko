f6250bde112cfae81011409ac85fce50b6832f42
devanamul <ahfahad118@gmail.com>
2025-05-12 09:33:03 +0600

    Created product controller on API and refactor the code

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,16 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreProductRequest;
 use App\Models\Product;
-use App\Models\ProductAttribute;
-use App\Models\ProductVariant;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Api\ProductController as ProductControllerApi;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 use Inertia\Inertia;
 
-class ProductController extends Controller
+class ProductController extends ProductControllerApi
 {
-    //
-
     public function index()
     {
         return Inertia::render('products/index', [
@@ -21,43 +18,24 @@ class ProductController extends Controller
         ]);
     }
 
+    /**
+     * @param StoreProductRequest $request
+     * @return JsonResponse|RedirectResponse
+     */
     public function store(StoreProductRequest $request)
     {
-        DB::beginTransaction();
-        try {
-            $product = new Product();
-            $product->name = $request->product_name;
-            $product->description = $request->product_description;
-            $product->hasVariations = $request->has_variations;
+        $response = parent::store($request);
 
-            if (!$product->save()) {
-                throw new \Exception('Product Not Saved');
-            }
-
-            foreach ($request->variants as $variant) {
-                $productAttribute = new ProductAttribute();
-                $productAttribute->product_id = $product->id;
-                $productAttribute->name = $variant['name'];
-                $productAttribute->options = $variant['options'];
-                $productAttribute->save();
-            }
-
-            foreach ($request->combinations as $combination) {
-                $variationName = $request->product_name . ' ' . implode(' ', array_values((array)$combination));
-                $productVariant = new ProductVariant();
-                $productVariant->product_id = $product->id;
-                $productVariant->name = $variationName;
-                $productVariant->options = $combination;
-                $productVariant->save();
-            }
-
-            DB::commit();
-            return redirect()->route('products.index')->with('success', 'Product created successfully.');
-
-        } catch (\Exception $e) {
-            DB::rollBack();
-
-            return back()->with('error', $e->getMessage());
+        if ($response->getData()->status === 'success')
+        {
+            return redirect()->route('products.index')->with('success', $response->getData()->message);
         }
+
+        elseif ($response->getData()->status === 'error')
+        {
+            return back()->with('error', $response->getData()->message);
+        }
+
+        return back()->with('error', 'Something went wrong');
     }
 }
