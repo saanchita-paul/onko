4132186a558dc9bfbbb942d00684fa0af832f3fe
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2025-05-12 13:44:24 +0600

    Add HasExpense trait, ExpenseFactory factory, modify DatabaseSeeder

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -2,11 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Expense extends Model
 {
+    use HasUuids, HasFactory;
+
     public function expensable(): MorphTo
     {
         return $this->morphTo();
