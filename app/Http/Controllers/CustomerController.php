c5fa71e5cb426a6345908baf3699b931457ef12c
devanamul <ahfahad118@gmail.com>
2025-05-22 17:12:39 +0600

    ONKO-21: work in progress

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -4,14 +4,60 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\SaveCustomerDetailsRequest;
 use App\Models\Customer;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Inertia\Inertia;
 
 class CustomerController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $range = $request->input('range');
+
+        $query = Customer::query();
+
+        if ($range && $range !== 'all'){
+            $today = Carbon::today();
+            switch ($range) {
+                case 'today':
+                    $currentFrom = Carbon::today()->startOfDay();
+                    $currentTo = Carbon::today()->endOfDay();
+                    break;
+                case 'week':
+                    $currentFrom = $today->copy()->startOfWeek(Carbon::SATURDAY);
+                    $currentTo = $today->copy()->endOfWeek(Carbon::FRIDAY);
+                    break;
+
+                case 'month':
+                    $currentFrom = $today->copy()->startOfMonth();
+                    $currentTo = $today->copy()->endOfMonth();
+                    break;
+
+                case 'quarter':
+                    $currentFrom = $today->copy()->startOfQuarter();
+                    $currentTo = $today->copy()->endOfQuarter();
+                    break;
+
+                case 'year':
+                    $currentFrom = $today->copy()->startOfYear();
+                    $currentTo = $today->copy()->endOfYear();
+                    break;
+                default:
+                    $currentFrom = null;
+                    $currentTo = null;
+                    break;
+            }
+
+            if ($currentFrom && $currentTo) {
+                $query->whereBetween('created_at', [
+                    $currentFrom->startOfDay(),
+                    $currentTo->endOfDay()
+                ]);
+            }
+        }
+
         return Inertia::render('customers/index', [
-            'customers' => Customer::paginate(25)
+            'customers' => $query->paginate(5)
         ]);
     }
 
