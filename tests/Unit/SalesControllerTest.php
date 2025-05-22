e97e633bec8e25d134b88fb57b69e69e20846540
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2025-05-22 15:00:44 +0600

    ONKO-35: Add more tests

--- tests/Unit/SalesControllerTest.php
+++ tests/Unit/SalesControllerTest.php
@@ -4,9 +4,7 @@ namespace Tests\Feature;
 
 use App\Models\Consignment;
 use App\Models\Order;
-use App\Models\Product;
 use App\Models\User;
-use Database\Factories\ConsignmentFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Log;
@@ -184,7 +182,7 @@ class SalesControllerTest extends TestCase
         Consignment::factory()->count(2)->create();
 
         $orders = Order::factory()
-            ->count(100)
+            ->count(50)
             ->create();
 
 
@@ -217,6 +215,112 @@ class SalesControllerTest extends TestCase
         $bestSellerByQty = $bestSellers->sortByDesc('total_qty')->values()->all();
         $bestSellerByValue = $bestSellers->sortByDesc('total_value')->values()->all();
         $response = $this->get(route('sales.index'));
+
+        $response->assertStatus(200);
+        $response->assertInertia(function($page) use ($bestSellerByQty, $bestSellerByValue){
+            $page->component('sales/index')
+                ->has('bQuantity', 10)
+                ->where('bQuantity.0.sum_qty', $bestSellerByQty[0]['total_qty'])
+                ->where('bQuantity.0.id', $bestSellerByQty[0]['product_id'])
+                ->has('bSubTotal', 10)
+                ->where('bSubTotal.0.id', $bestSellerByValue[0]['product_id']);
+        }); 
+
+        $this->assertTrue(true);
+
+        Order::factory()
+            ->count(50)
+            ->create([
+                'created_at' => Carbon::now()->addWeek(),
+            ]);
+
+        $response = $this->get(route('sales.index', ['range' => 'week']));
+
+        $response->assertInertia(function($page) use ($bestSellerByQty, $bestSellerByValue){
+            $page->component('sales/index')
+                ->has('bQuantity', 10)
+                ->where('bQuantity.0.sum_qty', $bestSellerByQty[0]['total_qty'])
+                ->where('bQuantity.0.id', $bestSellerByQty[0]['product_id'])
+                ->has('bSubTotal', 10)
+                ->where('bSubTotal.0.id', $bestSellerByValue[0]['product_id']);
+        });
+
+        $this->assertTrue(true);
+    }
+
+    public function test_best_sellers_for_week()
+    {   
+        $this->bestSellersTestHelper('week');
+    }
+
+    public function test_best_sellers_for_month()
+    {   
+        $this->bestSellersTestHelper('month');
+    }
+
+    public function test_best_sellers_for_quarter()
+    {   
+        $this->bestSellersTestHelper('quarter');
+    }
+
+    public function test_best_sellers_for_year()
+    {   
+        $this->bestSellersTestHelper('year');
+    }
+
+    private function bestSellersTestHelper($period)
+    {
+        $date = match ($period) {
+            'week' => Carbon::now()->addWeek(),
+            'month' => Carbon::now()->addMonth(),
+            'quarter' => Carbon::now()->addQuarter(),
+            'year' => Carbon::now()->addYear(),
+        };
+
+        $this->actingAs(User::factory()->create());
+
+        Consignment::factory()->count(2)->create();
+
+        $orders = Order::factory()
+            ->count(50)
+            ->create();
+
+        Order::factory()
+            ->count(50)
+            ->create([
+                'created_at' => $date,
+            ]);
+
+
+        $orders->load('orderItems.consignmentItem');
+
+        $orders = $orders->map(fn($order) => $order->orderItems);
+        $grouped  = $orders->collapse()->map(function($item) {
+
+            return [
+                'id' => $item->id,
+                'product_id' => $item->consignmentItem->product_id,
+                'qty' => $item->qty,
+                'unit_price' => intdiv($item->unit_price,100),
+                'subtotal' => $item->qty * intdiv($item->unit_price,100),
+            ];
+        })->groupBy('product_id');
+        $bestSellers = collect([]);
+
+        $grouped->each(function($item) use (&$bestSellers) {
+            $val = [
+                'product_id' => $item->first()['product_id'],
+                'total_qty' => $item->reduce(fn($carry, $value) => $carry + $value['qty'], 0),
+                'total_value' => $item->reduce(fn($carry, $value) => $carry + $value['subtotal'], 0)
+            ];
+
+            $bestSellers->push($val);
+
+        });
+
+        $bestSellerByQty = $bestSellers->sortByDesc('total_qty')->values()->all();
+        $bestSellerByValue = $bestSellers->sortByDesc('total_value')->values()->all();
+        $response = $this->get(route('sales.index', ['range' => $period]));
 
         $response->assertStatus(200);
         $response->assertInertia(function($page) use ($bestSellerByQty, $bestSellerByValue){
