d15e4764153af8e1f16c7938ce127b41f885dcca
sanchita paul <spaul@microsite.com>
2025-05-25 11:10:01 +0600

    [feature/ONKO-16/resize-invoice] request changes updated

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\Api;
 
 
+use App\Http\Requests\StoreOrderRequest;
+use App\Models\ConsignmentItem;
 use App\Models\Customer;
 use App\Models\Option;
 use App\Models\Order;
+use App\Models\OrderItem;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
@@ -43,5 +47,121 @@ class OrderController extends Controller
             'customers' => $customers
         ];
     }
+
+    public function getConfirmData(Request $request)
+    {
+        $companyDetails = Option::companyDetails();
+
+        return [
+            'customer' => $request->input('customer'),
+            'items' => $request->input('items'),
+            'companyDetails' => $companyDetails,
+            'orderId' => '',
+        ];
+    }
+
+    public function storeOrder(StoreOrderRequest $request)
+    {
+        $data = $request->validated();
+
+        try {
+            $order = null;
+
+            DB::transaction(function () use ($data, &$order) {
+                $order = Order::create([
+                    'customer_id' => $data['customer_id'],
+                    'sub_total' => $data['sub_total'],
+                    'grand_total' => $data['grand_total'],
+                    'discount_total' => 0,
+                    'tax_total' => 0,
+                    'payments_total' => 0,
+                    'status' => 'pending',
+                    'meta' => null,
+                ]);
+
+
+                foreach ($data['items'] as $item) {
+                    $consignmentItem = ConsignmentItem::where('product_id', $item['id'])->first();
+
+
+                    if (!$consignmentItem) {
+                        throw new \Exception("Consignment item not found for product ID: {$item['id']}");
+                    }
+
+                    if ($consignmentItem->qty < $item['qty']) {
+                        throw new \Exception("Not enough consignment quantity for product ID: {$item['id']}");
+                    }
+                    OrderItem::create([
+                        'order_id' => $order->id,
+                        'consignment_item_id' => $consignmentItem->id,
+                        'unit_price' => $item['price'],
+                        'qty' => $item['qty'],
+                        'status' => 'pending',
+                    ]);
+
+                    $consignmentItem->decrement('qty', $item['qty']);
+                    $consignmentItem->increment('qty_sold', $item['qty']);
+
+                    $product = Product::find($item['id']);
+                    if ($product) {
+                        if ($product->quantity < $item['qty']) {
+                            throw new \Exception("Not enough product quantity for product ID: {$item['id']}");
+                        }
+                        $product->decrement('quantity', $item['qty']);
+                    } else {
+                        throw new \Exception("Product not found with ID: {$item['id']}");
+                    }
+
+                }
+            });
+//            session()->forget('user_order_session');
+//            session()->forget('isReset');
+//            return redirect()->route('orders.show', $order->id);
+            return $order;
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', 'Failed to save order: ' . $e->getMessage())->withInput();
+        }
+    }
+
+    public function showOrder(Order $order)
+    {
+        $customer = $order->customer;
+        $productIds = [];
+        $productsQtyMap = [];
+        foreach ($order->orderItems as $orderItem) {
+            $productIds[] = $orderItem->consignmentItem->product_id;
+            $productsQtyMap[$orderItem->consignmentItem->product_id] = $orderItem->qty;
+        }
+        $items = [];
+        $products = Product::whereIn('id', $productIds)->get();
+        foreach ($products as $product) {
+            $items[] = [
+                'id' => $product->id,
+                'name' => $product->name,
+                'quantity' => $product->quantity,
+                'short_id' => $product->short_id,
+                'price' => $product->price,
+                'qty' => $productsQtyMap[$product->id]
+            ];
+        }
+        $companyDetails = Option::companyDetails();
+
+        return [
+            'customer' => $customer,
+            'items' => $items,
+            'companyDetails' => $companyDetails,
+            'orderId' => $order->id,
+        ];
+    }
+
+    public function resetSession()
+    {
+        session()->forget('user_order_session');
+
+        return [
+            'message' => 'Order session has been reset.',
+        ];
+    }
+
 
 }
--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,12 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreOrderRequest;
-use App\Models\ConsignmentItem;
-use App\Models\Option;
 use App\Models\Order;
-use App\Models\OrderItem;
-use App\Models\Product;
-use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 use App\Http\Controllers\Api\OrderController as ApiOrderController;
 use Illuminate\Http\Request;
@@ -39,75 +34,23 @@ class OrderController extends ApiOrderController
         return Inertia::render('orders/create', $data);
     }
 
-    public function confirm()
+    public function confirm(Request $request)
     {
-        $companyDetails = Option::companyDetails();
-        $sessionData = [
-            'customer' => request()->input('customer'),
-            'items' => request()->input('items'),
-            'companyDetails' => $companyDetails,
-            'orderId' => ''
-        ];
+        $sessionData = parent::getConfirmData($request);
         session(['user_order_session' => $sessionData]);
         return Inertia::render('orders/confirm-order', $sessionData);
+
+
     }
 
     public function store(StoreOrderRequest $request)
     {
-        $data = $request->validated();
-
         try {
-            $order = null;
+            $order = parent::storeOrder($request);
 
-            DB::transaction(function () use ($data, &$order) {
-                $order = Order::create([
-                    'customer_id' => $data['customer_id'],
-                    'sub_total' => $data['sub_total'],
-                    'grand_total' => $data['grand_total'],
-                    'discount_total' => 0,
-                    'tax_total' => 0,
-                    'payments_total' => 0,
-                    'status' => 'pending',
-                    'meta' => null,
-                ]);
-
-
-                foreach ($data['items'] as $item) {
-                    $consignmentItem = ConsignmentItem::where('product_id', $item['id'])->first();
-
-
-                    if (!$consignmentItem) {
-                        throw new \Exception("Consignment item not found for product ID: {$item['id']}");
-                    }
-
-                    if ($consignmentItem->qty < $item['qty']) {
-                        throw new \Exception("Not enough consignment quantity for product ID: {$item['id']}");
-                    }
-                    OrderItem::create([
-                        'order_id' => $order->id,
-                        'consignment_item_id' => $consignmentItem->id,
-                        'unit_price' => $item['price'],
-                        'qty' => $item['qty'],
-                        'status' => 'pending',
-                    ]);
-
-                    $consignmentItem->decrement('qty', $item['qty']);
-                    $consignmentItem->increment('qty_sold', $item['qty']);
-
-                    $product = Product::find($item['id']);
-                    if ($product) {
-                        if ($product->quantity < $item['qty']) {
-                            throw new \Exception("Not enough product quantity for product ID: {$item['id']}");
-                        }
-                        $product->decrement('quantity', $item['qty']);
-                    } else {
-                        throw new \Exception("Product not found with ID: {$item['id']}");
-                    }
-
-                }
-            });
             session()->forget('user_order_session');
             session()->forget('isReset');
+
             return redirect()->route('orders.show', $order->id);
         } catch (\Exception $e) {
             return redirect()->back()->with('error', 'Failed to save order: ' . $e->getMessage())->withInput();
@@ -116,39 +59,14 @@ class OrderController extends ApiOrderController
 
     public function show(Order $order)
     {
-        $customer = $order->customer;
-        $productIds = [];
-        $productsQtyMap = [];
-        foreach ($order->orderItems as $orderItem) {
-            $productIds[] = $orderItem->consignmentItem->product_id;
-            $productsQtyMap[$orderItem->consignmentItem->product_id] = $orderItem->qty;
-        }
-        $items = [];
-        $products = Product::whereIn('id', $productIds)->get();
-        foreach ($products as $product) {
-            $items[] = [
-                'id' => $product->id,
-                'name' => $product->name,
-                'quantity' => $product->quantity,
-                'short_id' => $product->short_id,
-                'price' => $product->price,
-                'qty' => $productsQtyMap[$product->id]
-            ];
-        }
-        $companyDetails = Option::companyDetails();
-
-        return Inertia::render('orders/view', [
-            'customer' => $customer,
-            'items' => $items,
-            'companyDetails' => $companyDetails,
-            'orderId' => $order->id,
-        ]);
+        $orderData = parent::showOrder($order);
+        return Inertia::render('orders/view', $orderData);
     }
 
 
     public function reset()
     {
-        session()->forget('user_order_session');
+        parent::resetSession();
         return redirect()->route('orders.create')->with('isReset', true);
     }
 
