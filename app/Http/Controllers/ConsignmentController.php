abbe99e18ddb353549b05713ac49b5f518baa8df
devanamul <ahfahad118@gmail.com>
2025-06-01 08:47:58 +0600

    Store controller created

--- app/Http/Controllers/ConsignmentController.php
+++ app/Http/Controllers/ConsignmentController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Consignment;
 use App\Http\Controllers\Api\ConsignmentController as ApiConsignmentController;
+use App\Models\ConsignmentItem;
+use http\Env\Request;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 class ConsignmentController extends ApiConsignmentController
@@ -15,5 +18,35 @@ class ConsignmentController extends ApiConsignmentController
         return Inertia::render('consignment/index', [
             'consignments' => $consignments
         ]);
+    }
+
+    public function store(Request $request){
+        $consignment = new Consignment();
+        try {
+            if($request->id){
+                $consignment->id = $request->id;
+            }
+            $consignment->lc_num = $request->lc_num;
+
+            if(!$consignment->save()){
+                throw new \Exception('Error saving consignment');
+            }
+
+            foreach ($request->items as $item){
+                $consignmentItem = new ConsignmentItem();
+                $consignmentItem->product_id = $item->product_id;
+                $consignmentItem->product_variant_id = $item->product_variant_id;
+                $consignmentItem->qty = $item->qty;
+                $consignmentItem->cost_price = $item->price;
+
+                $consignmentItem->save();
+            }
+
+            DB::commit();
+        }
+        catch (\Exception $exception){
+            DB::rollBack();
+            abort(500, $exception->getMessage());
+        }
     }
 }
