37e6f48767a8928b4861905bd6d516629069c0fa
sanchita <paul.sanchitaa@gmail.com>
2025-06-03 14:14:20 +0600

    [feature/ONKO-70-invoice-table-css] css issue

--- resources/js/pages/orders/create.tsx
+++ resources/js/pages/orders/create.tsx
@@ -392,7 +392,7 @@ export default function CreateOrder({ companyDetails, customers, userOrderSessio
                 <h1 className="text-2xl font-bold">New Invoice</h1>
                 <p className="text-muted-foreground mb-4">Create a new Invoice or Sales Receipt</p>
                 <div className="flex flex-col gap-6 lg:flex-row">
-                    <div className="w-full space-y-4 lg:flex-1">
+                    <div className="w-full lg:max-w-lvw space-y-4">
                         <Card>
                             <CardContent className="space-y-4 p-6 sm:p-6">
                                 <div className="grid grid-cols-2 gap-4">
